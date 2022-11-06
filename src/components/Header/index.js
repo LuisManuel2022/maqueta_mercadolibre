60897543ede8bb4cd334defe655f510304197d7b
Luis Manuel López Flores <105078499+LuisManuel2022@users.noreply.github.com>
2022-11-05 21:40:12 -0600

    Add files via upload

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container, Wrapper } from "./styles";
+
+export const Header = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <img
+          src="https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.19.5/mercadolibre/logo__large_plus.png"
+          alt="logo"
+        />
+        <div className="buscar">
+          <div className="input">
+            
+          </div>
+        </div>
+      </Wrapper>
+    </Container>
+  );
+};
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,1 +1,31 @@
+import styled from "styled-components";
 
+export const Container = styled.header`
+    width: 100vw;
+    background-color: var(--yellow);
+    height: 100px;
+`
+
+export const Wrapper = styled.nav`
+    max-width: 1200px;    
+    margin: 0 auto;
+    height: 91px;
+    display: flex;
+    img{
+        width: 134px;
+        height: 34px;
+        margin-top: 11px;
+        margin-left: 10px;
+    }
+    .buscar{
+        width: 600px;
+        height: 56px;
+        border: 1px solid red;
+        margin-left: 52px;        
+    }
+    .input{
+        width: 598px;
+        height: 39px;
+        border: 1px solid blue;
+    }
+`
