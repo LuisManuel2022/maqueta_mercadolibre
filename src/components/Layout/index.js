13c597634e780e38bdc4609837690259a97d0ecc
Luis Manuel López Flores <105078499+LuisManuel2022@users.noreply.github.com>
2022-11-05 21:45:02 -0600

    Add files via upload

---
+++ src/components/Layout/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Header } from "../Header";
+import { Hero } from "../Hero";
+import { Producto } from "../Producto";
+import {
+  Container,
+  Wrapper
+} from './styles'
+
+export const Layout = () => {
+  return (
+    <Container>
+      <Header />
+      <Wrapper>
+        <Hero />
+        <Producto />
+      </Wrapper>
+    </Container>
+  );
+};
